3a981a8b9b1fa050c13cf99786a088968fe829e9
Nick Zuber <nicholas@robinpowered.com>
2019-07-15 10:54:46 -0400

    Adjust space dots opacity

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const error = (msg) => {
+  console.error(`[GitHub Assistant] ${msg}`);
+}
+
 function copyText (text) {
   const x = window.scrollX, y = window.scrollY;
   const input = document.createElement('textarea');
@@ -14,37 +18,72 @@ function copyText (text) {
 }
 
 function addCopyAllButton () {
-  const node = document.querySelector('.text-gray.table-list-header-toggle');
-  const button = document.createElement('button');
-  button.setAttribute('type', 'button');
-  button.setAttribute('class', 'btn-link rgh-copy-all-selected pl-3');
-  button.innerText = 'Copy all';
-  button.onclick = () => {
-    var items = [];
-    document.querySelectorAll('.js-issues-list-check:checked').forEach(node => {
-      const div = node.parentElement.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling;
-      const anchor = div.childNodes[1];
-      const link = `https://github.com${anchor.getAttribute('href')}`;
-      const title = anchor.innerText;
-      items.push(link);
-    });
-    copyText(items.join('\n'));
+  try {
+    const node = document.querySelector('.text-gray.table-list-header-toggle');
+    const button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.setAttribute('class', 'btn-link rgh-copy-all-selected pl-3');
+    button.innerText = 'Copy all';
+    button.onclick = () => {
+      var items = [];
+      document.querySelectorAll('.js-issues-list-check:checked').forEach(node => {
+        const div = node.parentElement.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling;
+        const anchor = div.childNodes[1];
+        const link = `https://github.com${anchor.getAttribute('href')}`;
+        const title = anchor.innerText;
+        items.push(link);
+      });
+      copyText(items.join('\n'));
+    }
+    node.appendChild(button);
+  } catch (err) {
+    error('Error occurred while trying to add `Copy all` button.');
   }
-
-  node.appendChild(button);
 }
 
-function augmentUI () {
-  addCopyAllButton();
+function addStyleRule (selector, rule) {
+  document.styleSheets[0].insertRule(`${selector} ${rule}`, 0);
+}
+
+function addCopyAll (retry = 200, attempts = 0) {
+  if (attempts >= 10) {
+    error('Unable to add `Copy all` button.');
+  }
+  if (document.querySelector('.text-gray.table-list-header-toggle')) {
+    addCopyAllButton();
+  } else {
+    setTimeout(() => {
+      addCopyAll(retry * 2, ++attempts);
+    }, retry);
+  }
+}
+
+function adjustSpaceDots (retry = 200, attempts = 0) {
+  if (attempts >= 10) {
+    error('Unable to adjust the space-dots.');
+  }
+  if (document.querySelector('.rgh-space-char')) {
+    addStyleRule('.rgh-space-char', `{
+      opacity: 0.4;
+    }`);
+    addStyleRule('.rgh-ws-char', `{
+      opacity: 0.4;
+    }`);
+  } else {
+    setTimeout(() => {
+      adjustSpaceDots(retry * 2, ++attempts);
+    }, retry);
+  }
 }
 
 $(function () {
   // var observedNode = document.querySelector('body');
   // var config = { attributes: true, childList: true, subtree: true };
   // var observer = new MutationObserver((mutationsList, observer) => {
-  //   augmentUI();
+  //   addCopyAll();
   // });
   // observer.observe(observedNode, config);
 
-  augmentUI();
+  addCopyAll();
+  adjustSpaceDots();
 });
